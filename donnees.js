a42c9142b552fe2e633ad8c8a817107468a9e819
Djeffker <86652743+Djeffker@users.noreply.github.com>
2026-03-18 20:27:53 +0100

    Réorganisation chronologique des données

--- donnees.js
+++ donnees.js
@@ -1,6 +1,22 @@
 const ECTA_DATA = {
   "actualites": [
     {
+      "date": "Dimanche 5 Avril 2026",
+      "dateSort": "2026-04-05",
+      "titre": "Sortie du dimanche",
+      "texte": "Rendez-vous à 08h00 devant le Stade de foot. 2 Parcours : 83 et 70 kms",
+      "tag": "Sortie",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 29 Mars 2026",
+      "dateSort": "2026-03-29",
+      "titre": "Sortie du dimanche",
+      "texte": "Rendez-vous à 08h00 devant le Stade de foot. 2 Parcours : 90 et 76 kms",
+      "tag": "Sortie",
+      "image": ""
+    },
+    {
       "date": "Dimanche 22 Mars 2026",
       "dateSort": "2026-03-22",
       "titre": "Sortie du dimanche",
@@ -23,41 +39,9 @@ const ECTA_DATA = {
       "texte": "VTT (3 circuits de 25km à 45 km)\n\nRoute (50km et 70km)\n\nGravel (60km et 80km)\n\nMarche (8km et 12km) \n  \nLe départ des circuits sera donné à la salle Ondine au Grand Pré à partir de 8h30\n\nLes inscriptions seront prises à partir de 8h00 à la salle Ondine.",
       "tag": "Rando extérieure",
       "image": ""
-    },
-    {
-      "date": "Dimanche 29 Mars 2026",
-      "dateSort": "2026-03-29",
-      "titre": "Sortie du dimanche",
-      "texte": "Rendez-vous à 08h00 devant le Stade de foot. 2 Parcours : 90 et 76 kms",
-      "tag": "Sortie",
-      "image": ""
-    },
-    {
-      "date": "Dimanche 5 Avril 2026",
-      "dateSort": "2026-04-05",
-      "titre": "Sortie du dimanche",
-      "texte": "Rendez-vous à 08h00 devant le Stade de foot. 2 Parcours : 83 et 70 kms",
-      "tag": "Sortie",
-      "image": ""
     }
   ],
   "agenda": [
-    {
-      "date": "Dim. 22 mars.",
-      "dateSort": "2026-03-22",
-      "titre": "Circuit D25",
-      "locked": true,
-      "parcours": [
-        {
-          "distance": "91 Kms",
-          "lien": "https://www.openrunner.com/route-details/22909749"
-        },
-        {
-          "distance": "74 Kms",
-          "lien": "https://www.openrunner.com/route-details/17092617"
-        }
-      ]
-    },
     {
       "date": "Jeu. 5 mars.",
       "dateSort": "2026-03-05",
@@ -71,6 +55,22 @@ const ECTA_DATA = {
         {
           "distance": "70 Kms",
           "lien": "https://www.openrunner.com/route-details/23225269"
+        }
+      ]
+    },
+    {
+      "date": "Dim. 22 mars.",
+      "dateSort": "2026-03-22",
+      "titre": "Circuit D25",
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "91 Kms",
+          "lien": "https://www.openrunner.com/route-details/22909749"
+        },
+        {
+          "distance": "74 Kms",
+          "lien": "https://www.openrunner.com/route-details/17092617"
         }
       ]
     },
