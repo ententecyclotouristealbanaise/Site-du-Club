04cbeb771539408dbe0b0c1d424f5a5765466027
Djeffker <86652743+Djeffker@users.noreply.github.com>
2026-03-31 18:06:06 +0200

    Initial commit

---
+++ N2/donnees.js
@@ -0,0 +1,132 @@
+const ECTA_DATA = {
+  "actualites": [
+    {
+      "date": "Dimanche 22 Mars 2026",
+      "dateSort": "2026-03-22",
+      "titre": "RANDOS ENTRE TERRE ET BAIE  ( LANGUEUX )",
+      "texte": "VTT (3 circuits de 25km à 45 km) Route (50km et 70km) Gravel (60km et 80km) Marche (8km et 12km) Le départ des circuits sera donné à la salle Ondine au Grand Pré à partir de 8h30 Les inscriptions seront prises à partir de 8h00 à la salle Ondine.",
+      "tag": "Rando extérieure",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 22 Mars 2026",
+      "dateSort": "2026-03-22",
+      "titre": "Sortie du dimanche",
+      "texte": "Rendez-vous à 08h15 devant le Stade de foot. 2 Parcours : 91 et 74 kms",
+      "tag": "Sortie",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 22 Mars 2026",
+      "dateSort": "2026-03-22",
+      "titre": " Rando Amicale Cyclotourisme Broonaise ",
+      "texte": "L'Amicale Cyclotourisme Broonaise organise, le dimanche 22 mars 2026 (en matinée), une randonnée cyclotouriste et pédestre.\n\nLes cyclistes et marcheurs ont le choix entre plusieurs parcours avec ravitaillements.\nSur la route, qui conduira les cyclistes vers le Morbihan. 2 parcours de 57 km et 71 km sont proposés.\nLes 4 circuits VTT (25 km, 35 km, 40 km et 45 km) et la randonnée pédestre (7 km, 10 km et 14 km) empruntent principalement des chemins d’exploitation et sentiers du Bois de Broons.",
+      "tag": "Rando extérieure",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 29 Mars 2026",
+      "dateSort": "2026-03-29",
+      "titre": "Sortie du dimanche",
+      "texte": "Rendez-vous à 08h00 devant le Stade de foot. 2 Parcours : 90 et 76 kms",
+      "tag": "Sortie",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 5 Avril 2026",
+      "dateSort": "2026-04-05",
+      "titre": "Sortie du dimanche",
+      "texte": "Rendez-vous à 08h00 devant le Stade de foot. 2 Parcours : 83 et 70 kms",
+      "tag": "Sortie",
+      "image": ""
+    }
+  ],
+  "agenda": [
+    {
+      "date": "Dim. 22 mars.",
+      "dateSort": "2026-03-22",
+      "titre": "Circuit D25",
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "91 Kms",
+          "lien": "https://www.openrunner.com/route-details/22909749"
+        },
+        {
+          "distance": "74 Kms",
+          "lien": "https://www.openrunner.com/route-details/17092617"
+        }
+      ]
+    },
+    {
+      "date": "Dim. 29 mars.",
+      "dateSort": "2026-03-29",
+      "titre": "Circuit D53",
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "90 Kms",
+          "lien": "https://www.openrunner.com/route-details/20357096"
+        },
+        {
+          "distance": "76 Kms",
+          "lien": "https://www.openrunner.com/route-details/20357361"
+        }
+      ]
+    },
+    {
+      "date": "Dim. 5 avr..",
+      "dateSort": "2026-04-05",
+      "titre": "Circuit D63",
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "83 Kms",
+          "lien": "https://www.openrunner.com/route-details/23225098"
+        },
+        {
+          "distance": "70 Kms",
+          "lien": "https://www.openrunner.com/route-details/23225269"
+        }
+      ]
+    }
+  ],
+  "motDePasse": "ZWN0YTE5ODU=",
+  "galerie": [
+    {
+      "titre": "Galette des Rois 2026",
+      "couverture": "images/Galette des Rois 2026/photo_09.jpg",
+      "photos": [
+        "images/Galette des Rois 2026/photo_01.jpg",
+        "images/Galette des Rois 2026/photo_02.jpg",
+        "images/Galette des Rois 2026/photo_03.jpg",
+        "images/Galette des Rois 2026/photo_04.jpg",
+        "images/Galette des Rois 2026/photo_05.jpg",
+        "images/Galette des Rois 2026/photo_07.jpg",
+        "images/Galette des Rois 2026/photo_08.jpg",
+        "images/Galette des Rois 2026/photo_09.jpg",
+        "images/Galette des Rois 2026/photo_10.jpg",
+        "images/Galette des Rois 2026/photo_11.jpg",
+        "images/Galette des Rois 2026/photo_12.jpg",
+        "images/Galette des Rois 2026/photo_13.jpg",
+        "images/Galette des Rois 2026/photo_14.jpg",
+        "images/Galette des Rois 2026/photo_15.jpg",
+        "images/Galette des Rois 2026/photo_21.jpg"
+      ]
+    },
+    {
+      "titre": "Brevet des 50 Kms",
+      "couverture": "images/Brevet des 50 Kms/brevet 50_01.jpg",
+      "photos": [
+        "images/Brevet des 50 Kms/brevet 50_01.jpg",
+        "images/Brevet des 50 Kms/brevet 50_02.jpg",
+        "images/Brevet des 50 Kms/brevet 50_03.jpg",
+        "images/Brevet des 50 Kms/brevet 50_04.jpg",
+        "images/Brevet des 50 Kms/brevet 50_05.jpg",
+        "images/Brevet des 50 Kms/brevet 50_06.jpg",
+        "images/Brevet des 50 Kms/brevet 50_07.jpg",
+        "images/Brevet des 50 Kms/brevet 50_08.jpg"
+      ]
+    }
+  ]
+};
