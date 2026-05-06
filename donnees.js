1f1355d37542cb68b745fed886a7d190d958c7fc
ententecyclotouristealbanaise <ententecyclotouristealbanaise@gmail.com>
2026-05-06 09:10:23 +0200

    Add files via upload

---
+++ donnees.js
@@ -0,0 +1,130 @@
+const ECTA_DATA = {
+  "actualites": [
+    {
+      "date": "Mercredi 6 Mai 2026",
+      "dateSort": "2026-05-06",
+      "titre": "Débroussaillage rando de la Flora",
+      "texte": "Rendez-vous comme d'habitude 14 h devant le stade \n( Débroussaillage également les 6 et 13 mai aux même horaires )",
+      "tag": "Débroussaillage",
+      "image": ""
+    },
+    {
+      "date": "Vendredi 8 Mai 2026",
+      "dateSort": "2026-05-08",
+      "titre": "La Breizh Flandrienne Penguily",
+      "texte": "Vendredi 8 mai 2026 \nLa Breizh Flandrienne à Penguily \nPlusieurs circuits route et gravel\nPlus d'infos sur : https://www.penguily-cdf.fr/la-breizh-flandrienne",
+      "tag": "Rando extérieure",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 10 Mai 2026",
+      "dateSort": "2026-05-10",
+      "titre": "Sortie du dimanche (D19)",
+      "texte": "Départ à 8h devant le stade de foot \n2 circuits : 71 et 86  kms",
+      "tag": "Sortie",
+      "image": ""
+    }
+  ],
+  "agenda": [
+    {
+      "date": "Dim. 10 mai.",
+      "dateSort": "2026-05-10",
+      "titre": "Circuit  D19",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "71 Kms",
+          "lien": "https://www.openrunner.com/route-details/16755675"
+        },
+        {
+          "distance": "86 Kms",
+          "lien": "https://www.openrunner.com/route-details/16755980"
+        }
+      ]
+    }
+  ],
+  "motDePasse": "ZWN0YTE5ODU=",
+  "galerie": [
+    {
+      "titre": "Brevet des  100 Kms",
+      "couverture": "images/Brevet des 100 kms/brevet_100_05.jpg",
+      "aVenir": false,
+      "medias": [
+        {
+          "type": "image",
+          "src": "images/Brevet des 100 kms/brevet_100_01.jpg"
+        },
+        {
+          "type": "image",
+          "src": "images/Brevet des 100 kms/brevet_100_02.jpg"
+        },
+        {
+          "type": "image",
+          "src": "images/Brevet des 100 kms/brevet_100_03.jpg"
+        },
+        {
+          "type": "image",
+          "src": "images/Brevet des 100 kms/brevet_100_04.jpg"
+        },
+        {
+          "type": "image",
+          "src": "images/Brevet des 100 kms/brevet_100_05.jpg"
+        },
+        {
+          "type": "video",
+          "webm": "",
+          "mp4": "images/Brevet des 100 kms/videos/brevet_100_video_01.mp4",
+          "poster": "images/Brevet des 100 kms/videos/brevet_100_video_01_poster.jpg"
+        },
+        {
+          "type": "video",
+          "webm": "",
+          "mp4": "images/Brevet des 100 kms/videos/brevet_100_video_02.mp4",
+          "poster": "images/Brevet des 100 kms/videos/brevet_100_video_02_poster.jpg"
+        },
+        {
+          "type": "video",
+          "webm": "",
+          "mp4": "images/Brevet des 100 kms/videos/brevet_100_video_03.mp4",
+          "poster": "images/Brevet des 100 kms/videos/brevet_100_video_03_poster.jpg"
+        }
+      ]
+    },
+    {
+      "titre": "Galette des Rois 2026",
+      "couverture": "images/Galette des Rois 2026/photo_09.jpg",
+      "photos": [
+        "images/Galette des Rois 2026/photo_01.jpg",
+        "images/Galette des Rois 2026/photo_02.jpg",
+        "images/Galette des Rois 2026/photo_03.jpg",
+        "images/Galette des Rois 2026/photo_04.jpg",
+        "images/Galette des Rois 2026/photo_05.jpg",
+        "images/Galette des Rois 2026/photo_07.jpg",
+        "images/Galette des Rois 2026/photo_08.jpg",
+        "images/Galette des Rois 2026/photo_09.jpg",
+        "images/Galette des Rois 2026/photo_10.jpg",
+        "images/Galette des Rois 2026/photo_11.jpg",
+        "images/Galette des Rois 2026/photo_12.jpg",
+        "images/Galette des Rois 2026/photo_13.jpg",
+        "images/Galette des Rois 2026/photo_14.jpg",
+        "images/Galette des Rois 2026/photo_15.jpg",
+        "images/Galette des Rois 2026/photo_21.jpg"
+      ]
+    },
+    {
+      "titre": "Brevet des 50 Kms",
+      "couverture": "images/Brevet des 50 Kms/brevet 50_01.jpg",
+      "photos": [
+        "images/Brevet des 50 Kms/brevet 50_01.jpg",
+        "images/Brevet des 50 Kms/brevet 50_02.jpg",
+        "images/Brevet des 50 Kms/brevet 50_03.jpg",
+        "images/Brevet des 50 Kms/brevet 50_04.jpg",
+        "images/Brevet des 50 Kms/brevet 50_05.jpg",
+        "images/Brevet des 50 Kms/brevet 50_06.jpg",
+        "images/Brevet des 50 Kms/brevet 50_07.jpg",
+        "images/Brevet des 50 Kms/brevet 50_08.jpg"
+      ]
+    }
+  ]
+};
