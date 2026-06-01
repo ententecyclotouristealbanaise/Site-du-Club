29f2ebd1606613e51c119067b7eeb23a64fa13cd
ententecyclotouristealbanaise <ententecyclotouristealbanaise@gmail.com>
2026-06-01 08:12:17 +0200

    Add files via upload

---
+++ donnees.js
@@ -0,0 +1,58 @@
+const ECTA_DATA = {
+  "actualites": [
+    {
+      "date": "Dimanche 7 Juin 2026",
+      "dateSort": "2026-06-07",
+      "titre": "La Transménéene (Tredaniel)",
+      "texte": "Les départs s’effectueront à partir de 8 heures à allure libre de la salle Polyvalente de Trédaniel.\nVoici le lien pour imprimer le bulletin d'inscription pour ceux qui voudraient gagner du temps au départ 😁\nhttps://alslplemy.jimdofree.com/app/download/12525003121/bulletinparticipation2026.pdf?t=1776011440",
+      "tag": "Rando extérieure",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 7 Juin 2026",
+      "dateSort": "2026-06-07",
+      "titre": "Sortie du Dimanche (D22)",
+      "texte": "Départ 08H00  du Stade René Rouget.",
+      "tag": "Sortie",
+      "image": ""
+    }
+  ],
+  "agenda": [
+    {
+      "date": "Dim. 7 juin.",
+      "dateSort": "2026-06-07",
+      "titre": "Circuit  D22",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "69 Kms",
+          "lien": "https://www.openrunner.com/route-details/19083427"
+        },
+        {
+          "distance": "85 Kms",
+          "lien": "https://www.openrunner.com/route-details/19083878"
+        }
+      ]
+    },
+    {
+      "date": "Dim. 7 juin.",
+      "dateSort": "2026-06-07",
+      "titre": "La Transménéene 2026",
+      "videos": [],
+      "locked": false,
+      "parcours": [
+        {
+          "distance": "68 Kms",
+          "lien": "https://www.openrunner.com/route-details/23781002"
+        },
+        {
+          "distance": "92 Kms",
+          "lien": "https://www.openrunner.com/route-details/23781020"
+        }
+      ]
+    }
+  ],
+  "galerie": [],
+  "motDePasse": "ZWN0YTE5ODU="
+};
