500445a612a92768615679e2df61552fe24bb2c8
ententecyclotouristealbanaise <ententecyclotouristealbanaise@gmail.com>
2026-06-22 17:21:22 +0200

    Add files via upload

---
+++ donnees.js
@@ -0,0 +1,69 @@
+const ECTA_DATA = {
+  "actualites": [
+    {
+      "date": "Dimanche 28 Juin 2026",
+      "dateSort": "2026-06-28",
+      "titre": "Sortie du Dimanche ( D28 )",
+      "texte": "Départ 08H00 du Stade René Rouget",
+      "tag": "Sortie",
+      "image": ""
+    }
+  ],
+  "agenda": [
+    {
+      "date": "Dim. 28 juin.",
+      "dateSort": "2026-06-28",
+      "titre": "Circuit D 28",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "71 Kms",
+          "lien": "https://www.openrunner.com/route-details/17182353"
+        },
+        {
+          "distance": "93 Kms",
+          "lien": "https://www.openrunner.com/route-details/17183215"
+        }
+      ]
+    }
+  ],
+  "galerie": [
+    {
+      "titre": "Galette des Rois 2026",
+      "couverture": "images/Galette des Rois 2026/photo_09.jpg",
+      "photos": [
+        "images/Galette des Rois 2026/photo_01.jpg",
+        "images/Galette des Rois 2026/photo_02.jpg",
+        "images/Galette des Rois 2026/photo_03.jpg",
+        "images/Galette des Rois 2026/photo_04.jpg",
+        "images/Galette des Rois 2026/photo_05.jpg",
+        "images/Galette des Rois 2026/photo_07.jpg",
+        "images/Galette des Rois 2026/photo_08.jpg",
+        "images/Galette des Rois 2026/photo_09.jpg",
+        "images/Galette des Rois 2026/photo_10.jpg",
+        "images/Galette des Rois 2026/photo_11.jpg",
+        "images/Galette des Rois 2026/photo_12.jpg",
+        "images/Galette des Rois 2026/photo_13.jpg",
+        "images/Galette des Rois 2026/photo_14.jpg",
+        "images/Galette des Rois 2026/photo_15.jpg",
+        "images/Galette des Rois 2026/photo_21.jpg"
+      ]
+    },
+    {
+      "titre": "Brevet des 50 Kms",
+      "couverture": "images/Brevet des 50 Kms/brevet 50_01.jpg",
+      "photos": [
+        "images/Brevet des 50 Kms/brevet 50_01.jpg",
+        "images/Brevet des 50 Kms/brevet 50_02.jpg",
+        "images/Brevet des 50 Kms/brevet 50_03.jpg",
+        "images/Brevet des 50 Kms/brevet 50_04.jpg",
+        "images/Brevet des 50 Kms/brevet 50_05.jpg",
+        "images/Brevet des 50 Kms/brevet 50_06.jpg",
+        "images/Brevet des 50 Kms/brevet 50_07.jpg",
+        "images/Brevet des 50 Kms/brevet 50_08.jpg"
+      ]
+    }
+  ],
+  "motDePasse": "ZWN0YTE5ODU="
+};
