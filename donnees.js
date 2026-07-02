4aeca1525dfc805f4e7cf997614f3ae9a790e0b8
ententecyclotouristealbanaise <ententecyclotouristealbanaise@gmail.com>
2026-07-02 18:26:57 +0200

    Add files via upload

---
+++ donnees.js
@@ -0,0 +1,73 @@
+const ECTA_DATA = {
+  "actualites": [
+    {
+      "date": "Dimanche 5 Juillet 2026",
+      "dateSort": "2026-07-05",
+      "titre": "Sortie en LIgne + Circuit de remplacement ",
+      "texte": "2 distances Pour la sortie en ligne,  ainsi qu'un circuit de remplacement pour ceux qui n' y participe  pas .\n\nDépart du stade à 08H00",
+      "tag": "Sortie",
+      "image": ""
+    }
+  ],
+  "agenda": [
+    {
+      "date": "Dim. 5 juil..",
+      "dateSort": "2026-07-05",
+      "titre": "2 Circuit Sortie en Ligne / 1 Circuit de Remplacement",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "89 Kms",
+          "lien": "https://www.openrunner.com/route-details/24210732"
+        },
+        {
+          "distance": "73 Kms",
+          "lien": " https://www.openrunner.com/route-details/24209940"
+        },
+        {
+          "distance": "87 Kms  ( pour les non participants )",
+          "lien": " https://www.openrunner.com/route-details/24508605"
+        }
+      ]
+    }
+  ],
+  "galerie": [
+    {
+      "titre": "Galette des Rois 2026",
+      "couverture": "images/Galette des Rois 2026/photo_09.jpg",
+      "photos": [
+        "images/Galette des Rois 2026/photo_01.jpg",
+        "images/Galette des Rois 2026/photo_02.jpg",
+        "images/Galette des Rois 2026/photo_03.jpg",
+        "images/Galette des Rois 2026/photo_04.jpg",
+        "images/Galette des Rois 2026/photo_05.jpg",
+        "images/Galette des Rois 2026/photo_07.jpg",
+        "images/Galette des Rois 2026/photo_08.jpg",
+        "images/Galette des Rois 2026/photo_09.jpg",
+        "images/Galette des Rois 2026/photo_10.jpg",
+        "images/Galette des Rois 2026/photo_11.jpg",
+        "images/Galette des Rois 2026/photo_12.jpg",
+        "images/Galette des Rois 2026/photo_13.jpg",
+        "images/Galette des Rois 2026/photo_14.jpg",
+        "images/Galette des Rois 2026/photo_15.jpg",
+        "images/Galette des Rois 2026/photo_21.jpg"
+      ]
+    },
+    {
+      "titre": "Brevet des 50 Kms",
+      "couverture": "images/Brevet des 50 Kms/brevet 50_01.jpg",
+      "photos": [
+        "images/Brevet des 50 Kms/brevet 50_01.jpg",
+        "images/Brevet des 50 Kms/brevet 50_02.jpg",
+        "images/Brevet des 50 Kms/brevet 50_03.jpg",
+        "images/Brevet des 50 Kms/brevet 50_04.jpg",
+        "images/Brevet des 50 Kms/brevet 50_05.jpg",
+        "images/Brevet des 50 Kms/brevet 50_06.jpg",
+        "images/Brevet des 50 Kms/brevet 50_07.jpg",
+        "images/Brevet des 50 Kms/brevet 50_08.jpg"
+      ]
+    }
+  ],
+  "motDePasse": "ZWN0YTE5ODU="
+};
