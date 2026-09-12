19aa0668688924b3297606557d0df89024f2edc7
ententecyclotouristealbanaise <ententecyclotouristealbanaise@gmail.com>
2026-09-12 10:00:25 +0200

    Add files via upload

---
+++ donnees.js
@@ -0,0 +1,90 @@
+const ECTA_DATA = {
+  "actualites": [
+    {
+      "date": "Dimanche 13 Septembre 2026",
+      "dateSort": "2026-09-13",
+      "titre": "Trophée de la régularité ",
+      "texte": "Rendez-vous  pour 08h00 à la salle du mille- club boulevard Kennedy à Pléneuf Vall-André",
+      "tag": "Rando extérieure",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 13 Septembre 2026",
+      "dateSort": "2026-09-13",
+      "titre": "Sortie du Dimanche ( D61 )",
+      "texte": "Départ 08h00 au Stade ",
+      "tag": "Sortie",
+      "image": ""
+    }
+  ],
+  "agenda": [
+    {
+      "date": "Dim. 13 sept..",
+      "dateSort": "2026-09-13",
+      "titre": "D 61",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "66 Kms",
+          "lien": "https://www.openrunner.com/route-details/23215055"
+        },
+        {
+          "distance": "98 Kms",
+          "lien": "https://www.openrunner.com/route-details/23214846"
+        }
+      ]
+    },
+    {
+      "date": "Dim. 13 sept..",
+      "dateSort": "2026-09-13",
+      "titre": "Circuit de la régularité",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "45,82 Kms",
+          "lien": "https://www.openrunner.com/route-details/25055173"
+        }
+      ]
+    }
+  ],
+  "galerie": [
+    {
+      "titre": "Galette des Rois 2026",
+      "couverture": "images/Galette des Rois 2026/photo_09.jpg",
+      "photos": [
+        "images/Galette des Rois 2026/photo_01.jpg",
+        "images/Galette des Rois 2026/photo_02.jpg",
+        "images/Galette des Rois 2026/photo_03.jpg",
+        "images/Galette des Rois 2026/photo_04.jpg",
+        "images/Galette des Rois 2026/photo_05.jpg",
+        "images/Galette des Rois 2026/photo_07.jpg",
+        "images/Galette des Rois 2026/photo_08.jpg",
+        "images/Galette des Rois 2026/photo_09.jpg",
+        "images/Galette des Rois 2026/photo_10.jpg",
+        "images/Galette des Rois 2026/photo_11.jpg",
+        "images/Galette des Rois 2026/photo_12.jpg",
+        "images/Galette des Rois 2026/photo_13.jpg",
+        "images/Galette des Rois 2026/photo_14.jpg",
+        "images/Galette des Rois 2026/photo_15.jpg",
+        "images/Galette des Rois 2026/photo_21.jpg"
+      ]
+    },
+    {
+      "titre": "Brevet des 50 Kms",
+      "couverture": "images/Brevet des 50 Kms/brevet 50_01.jpg",
+      "photos": [
+        "images/Brevet des 50 Kms/brevet 50_01.jpg",
+        "images/Brevet des 50 Kms/brevet 50_02.jpg",
+        "images/Brevet des 50 Kms/brevet 50_03.jpg",
+        "images/Brevet des 50 Kms/brevet 50_04.jpg",
+        "images/Brevet des 50 Kms/brevet 50_05.jpg",
+        "images/Brevet des 50 Kms/brevet 50_06.jpg",
+        "images/Brevet des 50 Kms/brevet 50_07.jpg",
+        "images/Brevet des 50 Kms/brevet 50_08.jpg"
+      ]
+    }
+  ],
+  "motDePasse": "ZWN0YTE5ODU="
+};
