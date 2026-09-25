3eda188dfc76faad1a847701a51779a1ca1073d5
ententecyclotouristealbanaise <ententecyclotouristealbanaise@gmail.com>
2026-09-25 10:05:03 +0200

    Add files via upload

---
+++ donnees.js
@@ -0,0 +1,94 @@
+const ECTA_DATA = {
+  "actualites": [
+    {
+      "date": "Dimanche 27 Septembre 2026",
+      "dateSort": "2026-09-27",
+      "titre": "Virades de l' Espoir (Muco) Erquy",
+      "texte": "Le Dimanche 27 septembre,  la Virade d'Erquy vous accueille dès 8h du matin pour vivre un week-end sportif, festif et solidaire sur son fameux Port réputé pour ses coquilles Saint-Jacques. \nAnimation musicale, randonnées : \n\nPédestres 9km départ 8h30 et 7 km départ 9h. \nRando famille 3km départ 9h30. Marche Nordique rdv 9h départ 9h15. \nRando cyclo de 50 et 70 km (départ 8h30). \nRando moto (places limitées à 50) départ 10h.\n\n",
+      "tag": "Rando extérieure",
+      "image": ""
+    },
+    {
+      "date": "Dimanche 27 Septembre 2026",
+      "dateSort": "2026-09-27",
+      "titre": "Sortie du Dimanche (D 38)",
+      "texte": "Départ 08h15 du Stade",
+      "tag": "Sortie",
+      "image": ""
+    }
+  ],
+  "agenda": [
+    {
+      "date": "Dim. 27 sept..",
+      "dateSort": "2026-09-27",
+      "titre": "Circuits  D38",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "66 Kms",
+          "lien": "https://www.openrunner.com/route-details/17715037"
+        },
+        {
+          "distance": "74 Kms",
+          "lien": "https://www.openrunner.com/route-details/17715167"
+        }
+      ]
+    },
+    {
+      "date": "Dim. 27 sept..",
+      "dateSort": "2026-09-27",
+      "titre": "Virades de l'Espoir",
+      "videos": [],
+      "locked": true,
+      "parcours": [
+        {
+          "distance": "50 Kms",
+          "lien": "https://www.openrunner.com/route-details/5270792"
+        },
+        {
+          "distance": "70 Kms",
+          "lien": "https://www.openrunner.com/route-details/5270774"
+        }
+      ]
+    }
+  ],
+  "galerie": [
+    {
+      "titre": "Galette des Rois 2026",
+      "couverture": "images/Galette des Rois 2026/photo_09.jpg",
+      "photos": [
+        "images/Galette des Rois 2026/photo_01.jpg",
+        "images/Galette des Rois 2026/photo_02.jpg",
+        "images/Galette des Rois 2026/photo_03.jpg",
+        "images/Galette des Rois 2026/photo_04.jpg",
+        "images/Galette des Rois 2026/photo_05.jpg",
+        "images/Galette des Rois 2026/photo_07.jpg",
+        "images/Galette des Rois 2026/photo_08.jpg",
+        "images/Galette des Rois 2026/photo_09.jpg",
+        "images/Galette des Rois 2026/photo_10.jpg",
+        "images/Galette des Rois 2026/photo_11.jpg",
+        "images/Galette des Rois 2026/photo_12.jpg",
+        "images/Galette des Rois 2026/photo_13.jpg",
+        "images/Galette des Rois 2026/photo_14.jpg",
+        "images/Galette des Rois 2026/photo_15.jpg",
+        "images/Galette des Rois 2026/photo_21.jpg"
+      ]
+    },
+    {
+      "titre": "Brevet des 50 Kms",
+      "couverture": "images/Brevet des 50 Kms/brevet 50_01.jpg",
+      "photos": [
+        "images/Brevet des 50 Kms/brevet 50_01.jpg",
+        "images/Brevet des 50 Kms/brevet 50_02.jpg",
+        "images/Brevet des 50 Kms/brevet 50_03.jpg",
+        "images/Brevet des 50 Kms/brevet 50_04.jpg",
+        "images/Brevet des 50 Kms/brevet 50_05.jpg",
+        "images/Brevet des 50 Kms/brevet 50_06.jpg",
+        "images/Brevet des 50 Kms/brevet 50_07.jpg",
+        "images/Brevet des 50 Kms/brevet 50_08.jpg"
+      ]
+    }
+  ],
+  "motDePasse": "ZWN0YTE5ODU="
+};
